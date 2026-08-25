f99f59811c71e6fb2642e5a7e126df1731229057
CodeByAV <arpita.here1234@gmail.com>
2026-08-25 12:38:47 +0530

    Add files via upload
    added a calculator project!

---
+++ calculator.py
@@ -0,0 +1,25 @@
+print("MY FIRST CALCULATOR")
+
+A = float (input("enter the first number: "))
+operation = input("Enter the operation: ")
+B = float (input("enter the second number: "))
+
+if operation == '+':
+    print(A + B)
+elif operation == '-':
+    print(A - B)
+elif operation == '*':
+    print(A * B)
+elif operation == '/':
+    if B == 0:
+        print("Error: Division by zero is not allowed.")
+    else:
+        print(A / B)
+elif operation == '%':
+    if B == 0:
+        print("Error: Division by zero is not allowed.")
+    else:
+        print(A % B)
+elif operation == '**':
+    print(A ** B)
+print("Thank you for using the calculator!")
